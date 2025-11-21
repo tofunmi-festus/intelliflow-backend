33cc069dc0c1845d0873548f0298de7bb9f90b43
Festus <temiidowu321@gmail.com>
2025-11-21 12:25:36 +0100

    some changes

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ import  authMiddleware   from "./middlewares/authMiddleware";
 import { TransactionController } from "./controllers/TransactionController";
 
 const app = express();
+
 app.use(cors({
   origin: ["https://intelli-flow-frontend-r7wo.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
@@ -16,7 +17,20 @@ app.use(cors({
   optionsSuccessStatus: 200    // REQUIRED
 }));
 
-app.options("*", cors())
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://intelli-flow-frontend-r7wo.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "https://intelli-flow-frontend-r7wo.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.sendStatus(200);
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
